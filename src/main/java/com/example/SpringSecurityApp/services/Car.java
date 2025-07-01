f34e82a0c144d1f13171c069c93a141cb29d6fc9
Ibragim <gumakov111@gmail.com>
2025-07-01 20:29:56 +0300

    add date

--- src/main/java/com/example/SpringSecurityApp/services/Car.java
+++ src/main/java/com/example/SpringSecurityApp/services/Car.java
@@ -2,6 +2,9 @@ package com.example.SpringSecurityApp.services;
 
 public class Car {
     public static void main(String[] args) {
+        int[] ints = {10, 2, 3, 41, 10, 9, 4, -100, 10000};
+        int[] ints2 = {0, 22, 33, 141, 10, 99, 4, -1, 10000};
+        String string = "123";
         
     }
 }
